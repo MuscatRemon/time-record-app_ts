8e84560434a4bee8a2c41522f919fa16bc156575
MuscatRemon <tkrg0202@gmail.com>
2025-12-02 08:24:34 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button, HStack } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <div>hello world</div>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
+    </>
+  );
+}
+
+export default App;
