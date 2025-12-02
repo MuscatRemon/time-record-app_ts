edfb1575aacb675bb5637156e57c12e53263e3d9
MuscatRemon <tkrg0202@gmail.com>
2025-12-02 09:25:52 +0900

    firebaseデプロイ設定

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDh31di6DHfwjXyfEN8BFd0Kt-AT_AKvEE",
+  authDomain: "time-record-ts.firebaseapp.com",
+  projectId: "time-record-ts",
+  storageBucket: "time-record-ts.firebasestorage.app",
+  messagingSenderId: "135034188648",
+  appId: "1:135034188648:web:cc2c609f6852f403ff9cdd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
