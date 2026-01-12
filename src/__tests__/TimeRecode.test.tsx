f9d41d1f24fec2b26a29d7dbbcaac7216a07e0e5
MuscatRemon <tkrg0202@gmail.com>
2026-01-12 18:17:24 +0900

    vitestのmockエラー修正

--- src/__tests__/TimeRecode.test.tsx
+++ src/__tests__/TimeRecode.test.tsx
@@ -1,7 +1,40 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "../App.tsx";
 import { Provider } from "../components/ui/provider.tsx";
+
+let mockDB = [{ id: 1, title: "React", time: 2 }];
+
+vi.mock("@/utils/supabase", () => {
+  return {
+    default: {
+      from: vi.fn(() => ({
+        select: vi.fn().mockImplementation(async () => ({
+          data: [...mockDB],
+          error: null,
+        })),
+        // insert: vi.fn().mockImplementation(async ({ title, time }) => {
+        //   const newItem = {
+        //     id: mockDB.length + 1,
+        //     title: title,
+        //     time: Number(time),
+        //   };
+        //   mockDB.push(newItem);
+        //   return { data: [...mockDB], error: null };
+        // }),
+        // delete: vi.fn().mockImplementation(() => {
+        //   return {
+        //     eq: vi.fn(async (field, value) => {
+        //       const mocDBIndex = value - 1;
+        //       mockDB.splice(mocDBIndex, 1); // 削除
+        //       return { status: 204 };
+        //     }),
+        //   };
+        // }),
+      })),
+    },
+  };
+});
 
 describe("time-record-app-tsテスト", () => {
   it("h1が表示されている", () => {
